1cf01c1e2a4b6dcebbd269d8653f0ff3a053e934
Aleksandr Batollo <a@batollo.ru>
2023-10-08 20:08:32 +0300

    Добавит шаблоны и поменяет обработчики маршрутов

--- src/express/express.js
+++ src/express/express.js
@@ -1,16 +1,18 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 // Маршруты приложения мы опишем в отдельных файлах.
 // Для определения маршрутов мы воспользуемся Router().
 // Примеры маршрутов будут продемонстрированы ниже по тексту.
-const offersRoutes = require(`./routes/offer-routes`);
+const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 // Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
@@ -19,5 +21,13 @@ app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+
 // Запуск сервера
 app.listen(DEFAULT_PORT);
+
