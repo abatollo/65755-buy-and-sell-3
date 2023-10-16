09f4d9325e15339b7f12502c92eb7ab964f30ece
Aleksandr Batollo <a@batollo.ru>
2023-10-16 05:49:33 +0300

    Добавит идентификаторы, комментарии и реализует REST API

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,25 +2,28 @@
 
 const chalk = require(`chalk`);
 const express = require(`express`);
-const fs = require(`fs`).promises;
-const {HttpCode} = require(`../../constants`);
+// const fs = require(`fs`).promises;
+const {HttpCode, API_PREFIX} = require(`../../constants`);
+const routes = require(`../api`);
 
 
 const DEFAULT_PORT = 3000;
-const FILENAME = `mocks.json`;
+// const FILENAME = `mocks.json`;
 
 const app = express();
 app.use(express.json());
 
-app.get(`/offers`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (_err) {
-    res.send([]);
-  }
-});
+app.use(API_PREFIX, routes);
+
+// app.get(`/offers`, async (req, res) => {
+//   try {
+//     const fileContent = await fs.readFile(FILENAME);
+//     const mocks = JSON.parse(fileContent);
+//     res.json(mocks);
+//   } catch (_err) {
+//     res.send([]);
+//   }
+// });
 
 app.use((req, res) => res
   .status(HttpCode.NOT_FOUND)
@@ -32,14 +35,13 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(`Ошибка при создании сервера`, err);
-        }
+    app.listen(port, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`, err);
+      }
 
-        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      });
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+
+    });
   }
 };
