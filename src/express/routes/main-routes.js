9fa8c0353e39f6033b0e2df22067d596922318f5
Aleksandr Batollo <a@batollo.ru>
2023-10-21 02:48:45 +0300

    Добавит на страницы запрос и отправку данных

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,13 +3,32 @@
 // Подключаем и инициализируем экземпляр Router
 const {Router} = require(`express`);
 const mainRouter = new Router();
+const api = require(`../api`).getAPI();
 
 // Определяем `GET` маршруты.
 // В качестве ответа отправляем путь маршрута.
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, {offers});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
+
+mainRouter.get(`/search`, async (req, res) => {
+  try {
+    const {search} = req.query;
+    const results = await api.search(search);
+
+    res.render(`search-result`, {
+      results
+    });
+  } catch (arror) {
+    res.render(`search-result`, {
+      results: []
+    });
+  }
+});
 
 module.exports = mainRouter;
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -2,7 +2,28 @@
 
 // Подключаем и инициализируем экземпляр Router
 const {Router} = require(`express`);
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
+const {ensureArray} = require(`../../utils`);
+
+const UPLOAD_DIR = `../upload/img/`;
+
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+
+const api = require(`../api`).getAPI();
 const offersRouter = new Router();
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
 
 // Определяем `GET` маршруты.
 // В качестве ответа отправляем путь маршрута.
@@ -11,8 +32,45 @@ const offersRouter = new Router();
 // модуля маршрута в `express.js`.
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
+
 offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offers/ticket-edit`, {offer, categories});
+});
+
+offersRouter.post(`/add`,
+    upload.single(`avatar`), // применяем middleware
+    async (req, res) => {
+
+      // в `body` содержатся текстовые данные формы
+      // в `file` — данные о сохранённом файле
+      const {body, file} = req;
+      const offerData = {
+        picture: file ? file.filename : ``,
+        sum: body.price,
+        type: body.action,
+        description: body.comment,
+        title: body[`ticket-name`],
+        category: ensureArray(body.category),
+      };
+
+      console.log(offerData);
+
+      try {
+        await api.createOffer(offerData);
+        res.redirect(`/my`);
+      } catch (error) {
+        res.redirect(`back`);
+      }
+    }
+);
+
 offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
 
 module.exports = offersRouter;
